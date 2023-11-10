68ea849393c3524e485e2118ffe8c36d6db1e62d
Manish-choubey <manishchoubey7071@gmail.com>
2023-11-10 21:11:51 +0530

    implemented auth feature

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,18 @@
-import {Application} from 'express'
+import { Application } from 'express';
+import { authRoutes } from '../src/feature/auth/routes/authRoutes';
+import { serverAdapter } from '@service/queues/base.queue';
+import { currentUserRoutes } from '../src/feature/auth/routes/currentRoutes';
+import { authMiddleware } from '@global/helpers/auth-middleware';
 
+const BASE_PATH = '/api/v1';
 
-export default (app:Application)=>{
-    const routes = ()=>{
+export default (app: Application) => {
+  const routes = () => {
+    app.use('/queues', serverAdapter.getRouter());
+    app.use(BASE_PATH, authRoutes.routes());
+    app.use(BASE_PATH, authRoutes.signoutRoute());
 
-    }
-    routes()
-}
+    app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
+  };
+  routes();
+};
